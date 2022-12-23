c6097b925778ead85aec214bd30b32bddfee61dd
DurgaSandeshRasakatla <51785725+DurgaSandeshRasakatla@users.noreply.github.com>
2022-12-23 17:00:09 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,6 +5,15 @@
 #  client_id       = "4f6ca603-c73e-4c8a-8bcc-00265aaf104b"
 #  client_secret   = "240c61e6-b3cb-4945-b76d-15ad2b2ce6d5"
 #}
+#provider "azurerm" {
+#  features {}
+#}
+
+
 provider "azurerm" {
   features {}
+  subscription_id = "f8a53152-7fc9-4bac-b337-cacd2d85b0c8"
+  tenant_id       = "98f13429-d038-4e5e-85d8-846c6a963288"
+  client_id       = "02247a97-f8d7-4ce0-9892-a47a67376e6f"
+  client_secret   = "0b258af0-7ee1-4150-a4c3-c85bb10451c8"
 }
