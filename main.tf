cf106096af8b3e4b90ad4a80df8b238e27f6dd29
DurgaSandeshRasakatla <51785725+DurgaSandeshRasakatla@users.noreply.github.com>
2022-12-26 11:25:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
 # To run the Terraform config on localhost with the secrets:
 # terraform plan -var-file="secrets.tfvars"
+# terraform apply -var-file="secrets.tfvars"
 
+resource "azurerm_resource_group" "devopsdbrg" {
+  name     = var.rg_name
+  location = var.location
+}
 
 resource "azurerm_mysql_server" "devopsdatabase" {
   name                = var.db_name
-  location            = var.location
-  resource_group_name = var.rg_name
+  location            = azurerm_resource_group.devopsdbrg.location
+  resource_group_name = azurerm_resource_group.devopsdbrg.name
 
   administrator_login          = "mysqladmin"
   administrator_login_password = var.password
@@ -19,7 +24,8 @@ resource "azurerm_mysql_server" "devopsdatabase" {
   geo_redundant_backup_enabled      = false
   infrastructure_encryption_enabled = false
   public_network_access_enabled     = true
-  ssl_enforcement_enabled           = false
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
 
 
